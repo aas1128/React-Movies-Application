f5e455a2ef1563af447a4b26114ed6ba427d6290
Aayan Sayed <98489656+aas1128@users.noreply.github.com>
2025-02-19 14:20:28 -0500

    restarted project

--- React-Movie/src/pages/Favorites.jsx
+++
@@ -1,9 +0,0 @@
-import "../css/Favorites.css"
-
-const Favorites = () => {
-    return <h className="favorites-empty">
-        <h2>Hit the fav button on movies to display here</h2>
-    </h>
-}
-
-export default Favorites
